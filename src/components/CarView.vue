bff4519e2ee69d402aae0389b947b610dd81d75c
Diana <gatea.diana@gmail.com>
2023-05-17 14:50:52 +0300

    Initial commit

---
+++ src/components/CarView.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {useRoute, useRouter, RouterView} from "vue-router"
+import cars from "../data/cars.json"
+
+
+const route = useRoute();
+const router = useRouter();
+let carId= parseInt(route.params.id.toString());
+const car = cars.find(c => c.id === carId);
+const showContact = () => {
+  if (carId === 4) return;
+  return router.push(`/cars/${carId}/contact`);
+}
+
+const {price, name, year} = car; // Destructure properties
+</script>
+<template>
+  <div v-if="car">
+    <h1>Car View</h1>
+    <p>{{ name }}</p>
+    <p>{{ year }}</p>
+    <p>{{ price }}</p>
+    <button @click="showContact">Click for contact</button>
+    <RouterView/>
+  </div>
+  <div v-else>
+    <h1>Car not found</h1>
+  </div>
+</template>
+
---
+++ src/views/ContactView.vue
@@ -0,0 +1,14 @@
+<script setup>
+import {useRoute} from "vue-router";
+import cars from "../data/cars.json"
+const route=useRoute()
+ const car= cars.find(c =>  c.id === parseInt(route.params.id.toString()))
+const { contact, dealer, number } = car; // Destructure properties
+</script><template>
+<div  v-if="car">
+  <h1> Contact Info</h1>
+  <p>{{contact.dealer}}</p>
+  <p>{{contact.number}}</p>
+</div>
+</template>
+
